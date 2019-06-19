5881307e4ddc1fb81e1c0c6023bd846dbe7cb3c6
Boris Vinogradov <no111u3@gmail.com>
2019-06-19 22:14:20 +0300

    Update prelude and main module of library

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,18 @@
 #![no_std]
+#![allow(non_camel_case_types)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(deprecated)]
 
 pub use embedded_hal as hal;
 
-pub use stm32f7::stm32f7x7 as pac;
+pub use stm32f7::stm32f7x7 as stm32;
 
-pub use crate::pac as device;
-pub use crate::pac as stm32;
+pub use nb;
+pub use nb::block;
+
+#[cfg(feature = "rt")]
+pub use crate::stm32::interrupt;
 
 pub mod delay;
 pub mod gpio;
@@ -15,4 +22,8 @@ pub mod rcc;
 pub mod serial;
 pub mod spi;
 pub mod time;
-pub mod timer; 
+pub mod timer;
+// TODO: pub mod qei;
+// TODO: pub mod watchdog;
+// TODO: pub mod adc;
+// TODO: pub mod signature;
